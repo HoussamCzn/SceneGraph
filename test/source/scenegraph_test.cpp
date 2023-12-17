55eacb3e0d738ef098b79c6614e968d25a95d1e0
Houssam CUMZAIN <doriancoriace@gmail.com>
2023-12-17 10:05:12 +0100

    chore: remove auto-generated files

--- test/source/scenegraph_test.cpp
+++
@@ -1,8 +0,0 @@
-#include <catch2/catch_test_macros.hpp>
-#include <lib.hpp>
-
-TEST_CASE("Name is scenegraph", "[library]")
-{
-    auto const lib = library{};
-    REQUIRE(lib.name == "scenegraph");
-}
